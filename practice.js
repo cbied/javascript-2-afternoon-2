0f11cac7d1d73d129dbc29f856efe1ff874af315
Chris <cabiediger@gmail.com>
2019-04-30 17:46:23 -0500

    all test pass

--- practice.js
+++ practice.js
@@ -18,7 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
+function first(arr) {
+  return arr[0];
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -33,7 +35,10 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
+function last(arr) {
+  let lastItem = arr.length - 1;
+  return arr[lastItem];
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -48,7 +53,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
+function looper(family) {
+  for(let i=0; i<family.length; i++) {
+    alert(family[i]);
+  }
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -63,7 +72,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+function reversedLooper(letters) {
+  for(let i=letters.length - 1; i >= 0; i--) {
+    alert(letters[i]);
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -78,7 +91,17 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
+function evenFinder(nums) {
+  let evenNums = [];
+  for(let i=0; i < nums.length; i++) {
+    if(nums[i] % 2 == 0) {
+      evenNums.push(nums[i])
+    }
+  }
+  return evenNums
+}
 
+evenFinder(nums);
 
 
 
@@ -107,8 +130,23 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 */
 
 //Code Here
+function divider(numbersArray) {
+  let evenArr = [];
+  let oddArr = [];
+  for(let i=0; i < numbersArray.length; i++) {
+    if(numbersArray[i] % 2 === 0) {
+      evenArr.push(numbersArray[i])
+    } else {
+      oddArr.push(numbersArray[i])
+    }
+  }
 
+  let evenOddArr = [evenArr, oddArr];
 
+  return evenOddArr
+}
+
+divider(numbersArray)
 
 ////////// PROBLEM 7 //////////
 
@@ -129,7 +167,15 @@ var getRandomArbitrary = function() {
 */
 
 //Code Here
-
+function finder(arr) {
+  debugger;
+  let randomNumber = getRandomArbitrary();
+  if(arr.includes(randomNumber)) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 
 ////////// PROBLEM 8 //////////
@@ -158,8 +204,19 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 */
 
 //Code Here
+function removeItem(groceryList,item) {
+  let index = groceryList.indexOf(item);
+  if(index > 0 && Array.isArray(groceryList)) {
+    groceryList.splice(index,1);
+  } else {
+    groceryList = []
+  }
+ 
+   return groceryList
+}
 
 
+removeItem(myGroceryList, 'corndogs')
 
 ////////// PROBLEM 9 //////////
 
